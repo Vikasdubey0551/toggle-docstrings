8ef40804154a4d9139fb9d5202c1a5be4bee0132
vikasdubey0551 <vikas.iiserp@gmail.com>
2023-08-01 22:47:25 +0200

    Added logic to fold

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,90 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-  let isFoldActive: Boolean = false; 
 
-  context.subscriptions.push(vscode.commands.registerCommand('toggle-docstrings.hideDocstrings', () => {
-	vscode.commands.executeCommand('editor.foldAllBlockComments');
-	isFoldActive = true;
-	vscode.commands.executeCommand('setContext', 'isFoldActive', isFoldActive);
-	vscode.window.showInformationMessage('Docstrings hidden!');
+  let isFoldActive: boolean = false;
+
+  vscode.window.onDidChangeVisibleTextEditors((editors) => {
+    editors.forEach((editor) => {
+      const docUri = editor.document.uri;
+      vscode.commands.executeCommand('editor.unfoldAll');
+      vscode.commands.executeCommand('setContext', 'isFoldActive', false);
+    });
+  });
+  context.subscriptions.push(vscode.commands.registerCommand('toggle-docstrings.hideDocstrings', async () => {
+
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return;
+      }
+
+      const document = editor.document;
+      const languageId = document.languageId;
+
+      if (languageId !== 'python') {
+        return;
+      }
+
+      const docstrings: vscode.Selection[] = [];
+      let isInDocstring = false;
+      let isSingleLineDocstring = false;
+
+      for (let lineIndex = 0; lineIndex < document.lineCount; lineIndex++) {
+        const line = document.lineAt(lineIndex);
+        const lineText = line.text.trim();
+
+        if (lineText.startsWith('"""') || lineText.startsWith("'''")) {
+          isInDocstring = !isInDocstring;
+
+          if (isInDocstring) {
+            const startChar = lineText.indexOf('"""') + 3;
+            const endChar = lineText.length;
+            const startPos = new vscode.Position(lineIndex, startChar);
+            const endPos = new vscode.Position(lineIndex, endChar);
+            docstrings.push(new vscode.Selection(startPos, endPos));
+            isSingleLineDocstring = true;
+          }
+        } else if (isInDocstring && lineText.endsWith('"""') || lineText.endsWith("'''")) {
+          isSingleLineDocstring = false;
+          const startPos = new vscode.Position(lineIndex, 0);
+          const endPos = new vscode.Position(lineIndex, lineText.length);
+          docstrings.push(new vscode.Selection(startPos, endPos));
+        }
+
+        if (isInDocstring && isSingleLineDocstring) {
+          continue;
+        }
+
+        if (isInDocstring) {
+          const startPos = new vscode.Position(lineIndex, 0);
+          const endPos = new vscode.Position(lineIndex, lineText.length);
+          docstrings.push(new vscode.Selection(startPos, endPos));
+        }
+      }
+
+      editor.selections = docstrings;
+
+      await vscode.commands.executeCommand('editor.fold');
+
+      editor.selections = [];
+      if (docstrings.length > 0) {
+        const firstLine = docstrings[0].start.line;
+        const firstChar = docstrings[0].start.character;
+        const position = new vscode.Position(firstLine, firstChar);
+        editor.revealRange(new vscode.Range(position, position));
+        editor.selection = new vscode.Selection(position, position);
+      }
+
+    isFoldActive = true;
+    vscode.commands.executeCommand('setContext', 'isFoldActive', true);
+
   }));
 
-  context.subscriptions.push(vscode.commands.registerCommand('toggle-docstrings.showDocstrings', () => {
-    vscode.window.showInformationMessage('Docstrings are visible!');
-	isFoldActive = false;
-	vscode.commands.executeCommand('setContext', 'isFoldActive', isFoldActive);
-	vscode.commands.executeCommand('editor.unfoldAll');
+  context.subscriptions.push(vscode.commands.registerCommand('toggle-docstrings.showDocstrings', async () => {
+      isFoldActive = false;
+      vscode.commands.executeCommand('editor.unfoldAll');
+      vscode.commands.executeCommand('setContext', 'isFoldActive', false);
   }));
-
-  if (!isFoldActive) {
-    vscode.commands.executeCommand('toggle-docstrings.hideDocstrings');
-  } else {
-    vscode.commands.executeCommand('toggle-docstrings.showDocstrings');
-  }
 }
 
-export function deactivate() {
-  // Clean up resources when the extension is deactivated
-}
+export function deactivate() {}
